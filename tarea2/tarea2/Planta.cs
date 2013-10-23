c746accfc2f7ad09951f6dbe2b657b82393c4b28
emfrigo <emfrigo@hotmail.com>
2013-10-22 20:42:50 -0500

    Entrega de programa

---
+++ tarea2/tarea2/Planta.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace tarea2
+{
+	class Planta{
+
+		private string flor;
+		private string arbol;
+		private string color;
+
+		public virtual void cuidados ()
+		{
+
+	
+		}
+	}
+}
---
+++ tarea2/tarea2/Rosal.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace tarea2
+{
+	public class Rosal : Planta 
+	{
+		public string Rosas;
+		public string espinas;
+
+		public override void cuidados(){
+
+		}
+		public Rosal ()
+		{
+
+		}
+	}
+}
+
